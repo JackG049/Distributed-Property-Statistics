d4474a4e7694d59ef885e39e1d6c34cfb66368cd
Jack Geraghty <jack.geraghty@ucdconnect.ie>
2020-12-30 13:59:08 +0000

    Added images script and updated cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,9 @@
 echo "Cleaning up docker stuff..."
 docker-compose -f statistics/statistics_compose.yml down
-echo "Shutdown statistics service"
+docker-compose -f balancer/balancer-compose.yml down
+docker-compose -f client/client_compose.yml down
+docker-compose -f puller/puller_compose.yml down
+docker-compose -f dynamo-docker-compose.yml down
 echo "Shutting down kafka..."
 docker-compose down
 echo "Finished shutting down, have a nice day :)"
